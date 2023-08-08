52f4c906ea2103f0373bb002af375cd1cab7f719
karan26796 <karan26796@gmail.com>
2023-08-08 13:01:11 +0530

    Update actions.js

--- actions.js
+++ actions.js
@@ -1,19 +1,55 @@
 const text = document.querySelector(".k12");
-const strText = text.length;
+const strText = text.textContent.length;
 
 console.log(strText);
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+text.innerHTML = text.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-let animation = anime({
-	targets: '.k12',
-	opacity: 1,
-	translateY: 50, 
-	rotate: {
-		value: 360,
-		duration: 2000,
-		easing: 'easeInExpo'
-	}, 
-	scale: anime.stagger([0.7, 1], {from: 'center'}), 
-	delay: anime.stagger(100, {start: 1000}), 
-	translateX: [-10, 30]
-});
+// anime.timeline({loop: false})
+//   .add({
+//     targets: '.k12',
+//     translateY: [200,0],
+//     translateZ: 0,
+//     opacity: [0,1],
+//     easing: "easeOutExpo",
+//     duration: 2500,
+//     delay: function(el, i) { return i * 500; }
+//     // delay: (el, i) => 300 + 50 * i
+//   });
+
+//   var animation = anime({
+//     // targets:".container-home",
+//     translateY:600,
+//     opacity:1,
+//     easing:"easeInOutSine",
+//     autoPlay:false
+//   })
+
+//   window.onscroll=function(e){
+//     animation.seek(window.pageYOffset*2);
+//     console.log(window.pageYOffset);
+//   }
+
+  
+  animation.seek(100);
+
+//   anime.timeline({loop: false})
+//   .add({
+//     targets: '.container-home',
+//     translateY: [200,0],
+//     translateZ: 0,
+//     opacity: [0,1],
+//     easing: "easeOutExpo",
+//     duration: 2500,
+//     delay: function(el, i) { return i * 500; }
+//     // delay: (el, i) => 300 + 50 * i
+//   });
+
+//   var animation = anime({
+//     targets: '.play-pause-demo .el',
+//     translateX: 270,
+//     delay: function(el, i) { return i * 100; },
+//     direction: 'alternate',
+//     loop: true,
+//     autoplay: false,
+//     easing: 'easeInOutSine'
+//   });
